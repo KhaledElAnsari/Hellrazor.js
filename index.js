c645962121d70c1f3890c44b93b081130251ab88
Khaled Al-Ansari <khaledelansari@gmail.com>
2016-11-14 22:50:12 +0200

    first remote commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var Hellrazor = (function () {
+  this.createError = function(name, message) {
+    if(!name) {
+      throw new Error("name: is not defined");
+    }
+    else if(name.constructor !== String) {
+      throw new Error("name: is not of type String");
+    }
+
+    if(!message) {
+      throw new Error("message: is not defined");
+    }
+    else if(message.constructor !== String) {
+      throw new Error("message: is not of type String");
+    }
+
+    function customError(msg) {
+      if(msg) {
+        if(msg.constructor !== String) {
+          throw new Error("message: is not of type String");
+        }
+      }
+
+      this.name = name;
+      this.message = msg || message;
+    }
+    customError.prototype = new Error();
+
+    return customError;
+  };
+
+  return this;
+}());
+
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = Hellrazor;
+  }
+  exports.webility = Hellrazor;
+} else {
+  root.webility = Hellrazor;
+}
