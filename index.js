65295d26883bc4cc43e65a7387099972a8a3b4ae
Khaled Al-Ansari <khaledelansari@gmail.com>
2016-11-15 09:53:16 +0200

    replaced constructor with typeof for better type detection

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 var Hellrazor = (function () {
   this.createError = function(name, message) {
+    console.log(typeof name != "string");
     if(!name) {
       throw new Error("name: is not defined");
     }
-    else if(name.constructor !== String) {
+    else if(typeof name != "string") {
       throw new Error("name: is not of type String");
     }
 
     if(!message) {
       throw new Error("message: is not defined");
     }
-    else if(message.constructor !== String) {
+    else if(typeof message != "string") {
       throw new Error("message: is not of type String");
     }
 
     function customError(msg) {
       if(msg) {
-        if(msg.constructor !== String) {
+        if(typeof msg != "string") {
           throw new Error("message: is not of type String");
         }
       }
